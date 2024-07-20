8036d78ab4aa9434412f862778ac030088c3010e
Choi Danbi <eks4116@gmail.com>
2024-07-20 10:02:33 +0900

    init

---
+++ src/ju/Test.java
@@ -0,0 +1,7 @@
+package ju;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ src/yu/Test.java
@@ -0,0 +1,7 @@
+package yu;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
